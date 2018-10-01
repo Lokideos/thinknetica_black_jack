227d3643e65005a2c89aa5d478ca568ef8c48773
lokideos <archmage09@gmail.com>
2018-10-01 09:51:14 +0300

    Move suits&ranks to card class & move value determination to card class

--- app_data/card.rb
+++ app_data/card.rb
@@ -1,11 +1,32 @@
 # frozen_string_literal: true
 
 class Card
-  attr_accessor :rank, :suit, :value
+  attr_accessor :rank, :suit
+  attr_reader :value
 
-  def initialize(rank, suit, value)
+  class << self
+    SUITS = ["\u2660", "\u2665", "\u2663", "\u2666"].freeze
+    RANKS = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
+
+    def ranks
+      RANKS
+    end
+
+    def suits
+      SUITS
+    end
+  end
+
+  def initialize(rank, suit)
     @rank = rank
     @suit = suit
-    @value = value
+    @value = assign_value(rank)
+  end
+
+  def assign_value(rank)
+    value = 11
+    value = rank.to_i if rank.to_i != 0
+    value = 10 if rank.class.to_s == "String" && rank != "A"
+    value
   end
 end
--- app_data/deck.rb
+++ app_data/deck.rb
@@ -5,10 +5,6 @@ require_relative "card"
 class Deck
   attr_accessor :cards
 
-  SUITS = ["\u2660", "\u2665", "\u2663", "\u2666"].freeze
-  RANKS_WITH_VALUES = [["2", 2], ["3", 3], ["4", 4], ["5", 5], ["6", 6], ["7", 7], ["8", 8],
-                       ["9", 9], ["10", 10], ["J", 10], ["Q", 10], ["K", 10], ["A", :a]].freeze
-
   def initialize
     @cards = fill_deck
   end
@@ -18,9 +14,9 @@ class Deck
   end
 
   def fill_deck
-    RANKS_WITH_VALUES.each_with_object([]) do |rank, a|
-      SUITS.each do |suit|
-        a << Card.new(rank[0], suit, rank[1])
+    Card.ranks.each_with_object([]) do |rank, a|
+      Card.suits.each do |suit|
+        a << Card.new(rank, suit)
       end
     end.shuffle!
   end
